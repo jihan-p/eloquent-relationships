26bfa1bd3b8f17b58adf1348807bf9c30d3f5d24
jihan-p <jihan.pahlevi@smkn-2sbg.sch.id>
2025-01-13 16:07:07 +0700

    Membuat relasi One to Many

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    /**
+     * post
+     *
+     * @return void
+     */
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    /**
+     * comments
+     *
+     * @return void
+     */
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
