08ec914eba550faf50c53a0e882f0c05f43c8055
jihan-p <jihan.pahlevi@smkn-2sbg.sch.id>
2025-01-13 15:36:12 +0700

    Menambah Route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/**
+ * @path /users
+ * @method GET
+ */
+Route::get('/users', [\App\Http\Controllers\UserController::class, 'index']);
